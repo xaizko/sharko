5da3274889d6e50df3f923a6185b815b7a5ff328
xaizko <tonytran177013@gmail.com>
2025-09-18 16:09:11 -0400

    set up child bash process via pty

--- sharko.c
+++ sharko.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pty.h>
+#include <unistd.h>
 
+static int32_t masterfd;
 int main() {
-    printf("Hello world\n");
+
+    if (forkpty(&masterfd, NULL, NULL, NULL) == 0) {
+	execlp("/usr/bin/bash", "bash", NULL);
+	perror("execlp");
+	exit(1);
+    }
 
     return 0;
 }
