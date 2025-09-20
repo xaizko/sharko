63d3b4f21c762456e4037b2f50d20d8b9a60b2e1
xaizko <tonytran177013@gmail.com>
2025-09-19 20:03:50 -0400

    preparing for reading

--- sharko.c
+++ sharko.c
@@ -12,5 +12,20 @@ int main() {
 	exit(1);
     }
 
+    bool running = true;
+
+    fd_set fdset;
+    while(running) {
+	FD_ZERO(&fdset); // zero out set
+	FD_SET(masterfd, &fdset); // watch masterfd for reading
+
+	select(masterfd + 1, &fdset, NULL, NULL, NULL); // wait until reading
+
+	// FD_ISSET lets you know there is data to be read
+	if (FD_ISSET(masterfd, &fdset)) {
+	    //readfrompty();
+	}
+    }
+
     return 0;
 }
